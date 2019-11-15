1904e44d9d799b338630fa8cc526e2d84e5a43e8
Ivan Klimberg <ivanklimberg@gmail.com>
2019-11-15 20:17:24 -0300

    creating gitignore file and adding import to index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import base from './conn/base';
+import micuenta from './conn/micuenta';
+import operar from './conn/operar';
+import titulos from './conn/titulos';
+
+export default { 
+    base,
+    micuenta,
+    operar,
+    titulos
+}
